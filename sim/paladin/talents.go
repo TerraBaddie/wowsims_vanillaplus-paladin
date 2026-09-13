18c2ffdcb14ab2830505e7e3e9d571b36b760865
TerraBaddie <45601356+TerraBaddie@users.noreply.github.com>
2026-09-13 16:51:53 -0400

    Paladin: fix VanillaPlus two-handed weapon specialization scaling

--- sim/paladin/talents.go
+++ sim/paladin/talents.go
@@ -146,9 +146,11 @@ func (paladin *Paladin) applyReckoning() {
 func (paladin *Paladin) getWeaponSpecializationModifier() float64 {
 	handType := paladin.MainHand().HandType
 	if handType == proto.HandType_HandTypeMainHand || handType == proto.HandType_HandTypeOneHand {
+		// VanillaPlus One-Handed Weapon Specialization: +2/4/6/8/10% damage.
 		return 1. + 0.02*float64(paladin.Talents.OneHandedWeaponSpecialization)
 	} else if handType == proto.HandType_HandTypeTwoHand {
-		return 1. + 0.02*float64(paladin.Talents.TwoHandedWeaponSpecialization)
+		// VanillaPlus Two-Handed Weapon Specialization is +4/7/10%, not +2% per rank.
+		return []float64{1.0, 1.04, 1.07, 1.10}[paladin.Talents.TwoHandedWeaponSpecialization]
 	} else {
 		return 1.
 	}
@@ -222,9 +224,9 @@ func (paladin *Paladin) applyVindication() {
 			paladin.DisableDynamicStatDep(sim, vindicationMultiplier[paladin.Talents.Vindication])
 		},
 	})
-	// 	vindicationAuras := paladin.NewEnemyAuraArray(func(target *core.Unit) *core.Aura {
-	// 		return core.VindicationAura(target, paladin.Talents.Vindication)
-	// 	})
+	// \tvindicationAuras := paladin.NewEnemyAuraArray(func(target *core.Unit) *core.Aura {
+	// \t\treturn core.VindicationAura(target, paladin.Talents.Vindication)
+	// \t})
 	paladin.RegisterAura(core.Aura{
 		Label:    "Vindication Talent",
 		Duration: core.NeverExpires,
